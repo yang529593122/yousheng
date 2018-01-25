ce719d45ac7c9ffcc91cc33163e056ae22f5808d
Qingjiaowomihuxiaojie <>
2018-01-25 14:05:24 +0800

    推到了我的分支上

--- src/components/header/tab.vue
+++ src/components/header/tab.vue
@@ -19,11 +19,11 @@
 		data(){
 			return {
 				data:[
-					{title:"智能电器",item:["sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf"]},
-					{title:"健康生活",item:["sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf"]},
-					{title:"母婴精选",item:["sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf"]},
-					{title:"个人洗护",item:["sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf"]},
-					{title:"饮食特产",item:["sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf"]},
+					{title:"智能电器",item:["电视/音响","厨房电器","居家电器","大家电","电脑数码","个人用品","热水器","取暖器"]},
+					{title:"健康生活",item:["营养保健","滋补养生","儿童玩具","体育运动","生活用品","服饰","公交一卡通","定制交通卡"]},
+					{title:"母婴精选",item:["婴儿奶粉","尿裤/湿巾","婴幼用品","营养辅食","妈妈用品"]},
+					{title:"个人洗护",item:["面部护肤","香水、精油","男士护理","女性护理","沐浴、美发","口腔护理"]},
+					{title:"饮食特产",item:["茶叶茗品","休闲零食","精品酒水","饮料、咖啡","进口食品","生鲜、水果","孔哥休闲零食","速食、特产","粮油、杂货","若谷草堂"]},
 					{title:"员工福利",item:["sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf"]},
 					{title:"企业专属",item:["sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf"]},
 					{title:"公益活动",item:["sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf"]}
